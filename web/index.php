1e50bbfe3dd4eae446d24e06509be9c577f99ec1
drewbrenet <drew-brenet.oispuu@khk.ee>
2018-09-13 09:57:53 +0300

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(new Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__.'/../views',
+]);
+$app->get('/bookings/create', function () use ($app) {
+    return $app['twig']->render('base.html.twig');
+});
+$app->run();
+?>
